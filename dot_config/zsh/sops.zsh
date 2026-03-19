0a06a917f64881da6c1b1c67985d32825559abbd
Eric Austin <eric.m.austin@gmail.com>
2026-03-19 09:05:18 -0500

    updated zsh

---
+++ dot_config/zsh/sops.zsh
@@ -0,0 +1,4 @@
+SOPS_CRYPT_SECRET_SUFFIX="sops.dec"
+SOPS_CRYPT_ENCRYPTED_INFIX="sops.enc"
+SOPS_CRYPT_SEARCH_TOOL="fd"
+SOPS_CRYPT_FILE_PATTERNS=("*.yaml" "*.yml" "*.json" "*.env" "*.txt" "*.ini" "*.conf" "*.toml" "*.xml" "*.md")
